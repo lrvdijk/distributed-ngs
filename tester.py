c8c55664999df9008d97bd37ba1d8e520d5dfef8
Lucas van Dijk <info@return1.net>
2016-04-21 15:22:18 +0200

    Use relative data path

--- tester.py
+++ tester.py
@@ -1,19 +1,13 @@
 import sys
 import socket
 import os
-import time
 import json
-
-from digs.manager.models import DataType
 
 
 def main():
-    hash = 11111
+    hash = 11011
     str1 = 'store_data '
-    data = {}
-    data['hash'] = hash
-    data['size'] = 5953039
-    data['type'] = 'shotgun'
+    data = {'hash': hash, 'size': 59539, 'type': 'shotgun'}
     str1 = str1 + json.dumps(data) + '\n'
     print(str1)
 
@@ -40,16 +34,18 @@ def main():
     result = json.loads(parts[1])
     print(result['socket'])
     cmd = "rsync -avz --append-verify --info=progress2 "
-    local_path = "/home/dwarrel/Courses/Distributed/distributed-ngs/DataFiles/ClientData/AAAE "
-    remote_path = "/home/dwarrel/Courses/Distributed/distributed-ngs/DataFiles/DataNodes/" + str(result['socket'])
-    rsync = cmd + local_path + remote_path
+
+    base_path = os.path.join(os.path.dirname(__file__), 'DataFiles')
+
+    local_path = os.path.join(base_path, 'ClientData', 'AAAE')
+    remote_path = os.path.join(base_path, 'DataNodes', str(result['socket']))
+    rsync = "{} {} {}".format(cmd, local_path, remote_path)
     err = os.system(rsync)
     print(err)
     if err is 0:
         print("No error found! good job")
         str1 = 'store_data_done '
-        data = {}
-        data['hash'] = hash
+        data = {'hash': hash}
         str1 = str1 + json.dumps(data) + '\n'
         family, type, proto, _, addr = socket.getaddrinfo(
             host, port, proto=socket.IPPROTO_TCP)[0]
@@ -61,7 +57,6 @@ def main():
 
     else:
         print("Error rsync not complete! try again!")
-
 
 
 if __name__ == '__main__':
