e90a86d7317c8c248fea50aa3277de8483b164af
Lucas van Dijk <info@return1.net>
2016-04-22 12:00:38 +0200

    Add FASTQ split offsets action on data node

--- digs/data_node/handlers.py
+++ digs/data_node/handlers.py
@@ -4,17 +4,22 @@ Data Files Management
 
 This module provides some helper functions to manage files.
 """
+
 import os
 import logging
 
+
 from digs.exc import InvalidChunkSizeError
-from digs.data_node.actions import (parser, HeartBeat, GetDataChunk)
+from digs.common.actions import GetDataChunk, FindOffsetsFASTQ, ChunkOffsets
+from digs.messaging.protocol import DigsProtocolParser
 
 
 logger = logging.getLogger(__name__)
 
+transient_parser = DigsProtocolParser()
 
-@parser.register_handler(GetDataChunk)
+
+@transient_parser.register_handler(GetDataChunk)
 async def get_data_chunk(protocol, action):
     """Get a specific substring of a data file. Returned value in bytes.
 
@@ -37,12 +42,60 @@ async def get_data_chunk(protocol, action):
             "Invalid chunk size (zero or smaller)."
         )
 
-    f = open(file_path, 'rb')
-    f.seek(chunk_start)
-    chunk_data = f.read(chunk_end-chunk_start)
+    with open(file_path, 'rb') as f:
+        f.seek(chunk_start)
+        chunk_data = f.read(chunk_end-chunk_start)
 
-    return chunk_data
+        protocol._stream_writer.write(chunk_data)
+        await protocol._stream_writer.drain()
 
 
+def _calculate_fastq_offsets(fh, chunk_size=64):
+    """Parses a FASTQ file and determines proper splitting byte offsets,
+    while keeping the records complete. This function does not do a lot of
+    error checking: we assume the file is properly formatted.
+
+    It simply checks if a line starts with a '@', which indicates the start
+    of a new record, see if we exceed the current chunk size, and store the
+    chunk start and end positions when necessary.
+
+    :param fh: Open file handle to the FASTQ file
+    :type fh: file
+    :param chunk_size: Approximate size of each chunk in MB
+    :type chunk_size: int
+    """
+    fh.seek(0)
+    current_chunk_start = 0
+
+    # Cannot use normal iteration, because then telling the cursor position
+    # is disabled
+    while True:
+        # Ok, we've come across a quality score header, now parse the
+        # quality scores until the next record.
+        pos = fh.tell()
+        line = fh.readline()
+
+        if not line:
+            # EOF
+            break
+
+        if line.startswith('@'):
+            current_chunk_size = pos - current_chunk_start
+
+            if current_chunk_size >= chunk_size * 1024**2:
+                yield (current_chunk_start, pos)
+                current_chunk_start = pos
 
 
+@transient_parser.register_handler(FindOffsetsFASTQ)
+async def find_offsets_fastq(protocol, action):
+    """Determine sensible byte offsets for splitting up a fastq file,
+    while maintaining the records."""
+
+    file_path = action['file_path']
+
+    with open(file_path) as f:
+        chunks = list(_calculate_fastq_offsets(f))
+
+        action = ChunkOffsets(offsets=chunks)
+        await protocol.send_action(action)
