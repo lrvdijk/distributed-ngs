9e49bbbdb56439ebb2e5ee67c5622649bcd4d07e
Lucas van Dijk <info@return1.net>
2016-04-26 13:42:38 +0200

    Add initial code for worker node

--- digs-worker-node.py
+++ digs-worker-node.py
@@ -1,5 +1,9 @@
 import argparse
 
+from digs import conf
+from digs.common.utils import get_random_central_server
+from digs.exc import ConfigurationError
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -14,6 +18,19 @@ def main():
     )
 
     args = parser.parse_args()
+
+    if 'worker' not in conf.settings:
+        raise ConfigurationError("The configuration file does not have a "
+                                 "'worker' section.")
+    worker_settings = conf.settings['worker']
+
+    rabbitmq_settings = {
+        key.replace("rabbitmq.", ""): worker_settings[key]
+        for key in worker_settings if key.startswith("rabbitmq.")
+        }
+
+    rabbitmq_settings['host'] = get_random_central_server()
+    rabbitmq_settings['port'] = 5672
 
 
 
