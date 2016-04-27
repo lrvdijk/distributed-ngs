75eeacb5e0b3846ef158dae317ada293242699ba
Tycho Marinus <mail@tmarinus.nl>
2016-04-27 14:56:53 +0200

    HOTFIX startup-script

--- startup-for-nodes-gcloud.sh
+++ startup-for-nodes-gcloud.sh
@@ -6,15 +6,20 @@ fi
 cat <<EOF > /test
 check
 EOF
+# Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
+exec > >(tee -i logfile.txt)
+exec 2>&1
 
 mkdir /distributed
 cd /distributed
 git clone https://github.com/sh4wn/distributed-ngs.git
+#TMP go to postgresql branch
+cd distributed-ngs
 git checkout postgreSQL
 export LC_ALL=C
-apt-get install -y erlang-nox python3-pip libpq-dev python-dev python-sqlalchemy postgresql postgresql-contrib language-pack-en
+apt-get install -y erlang-nox python3-pip libpq-dev python-dev python-sqlalchemy postgresql postgresql-contrib
 locale-gen en_US.UTF-8
-service postgresql status
+service postgresql restart
 
 wget https://www.rabbitmq.com/releases/rabbitmq-server/v3.6.1/rabbitmq-server_3.6.1-1_all.deb
 dpkg -i rabbitmq-server_3.6.1-1_all.deb
@@ -22,6 +27,7 @@ apt-get -y -f install
 service rabbitmq-server restart
 cd distributed-ngs/
 export LC_ALL=C
+echo "LC_ALL=en_GB.utf8" >> /etc/environment
 python3 -m pip install aioamqp
 python3 -m pip install sqlalchemy
 python3 -m pip install psycopg2
