ffce7931592ac2016dd7feee106ddc5bba9e8758
Lucas van Dijk <info@return1.net>
2016-05-05 21:19:51 +0200

    Add debug mode to worker node

--- digs-worker-node.py
+++ digs-worker-node.py
@@ -1,11 +1,16 @@
+import logging
 import argparse
 import asyncio
+import warnings
 
 from digs import conf
 from digs.common.utils import get_random_central_server
 from digs.exc import ConfigurationError
 from digs.worker.handlers import WorkerProtocol
 from digs.messaging import persistent
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def main():
@@ -35,10 +40,17 @@ def main():
     rabbitmq_settings['port'] = 5672
 
     loop = asyncio.get_event_loop()
+
+    if args.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+        warnings.filterwarnings("always", category=ResourceWarning)
+        loop.set_debug(True)
+
     coro = persistent.create_persistent_listener(WorkerProtocol,
                                                  **rabbitmq_settings)
     listener = loop.run_until_complete(coro)
     loop.create_task(listener.basic_consume("jobs.mafft"))
+    logger.
 
     try:
         loop.run_forever()
